fa181ddb6ca19a7aa288b833fc9734cc41ccbc70
Ayn <aynuddin95@gmail.com>
2026-08-13 15:03:14 +0530

    My first tutorials

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let value = "Ayn Uddin started doing TypeScript and Playwright on live coding session";
+console.log(value);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let value: string = "Ayn Uddin started doing TypeScript and Playwright on live coding session";
+console.log(value);
